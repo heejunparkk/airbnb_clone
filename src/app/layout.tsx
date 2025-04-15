2f0a8dbe9ced1b42f04e043420c538171c460f35
heejunparkk <gmlwns2002@gmail.com>
2025-04-15 13:48:57 +0900

    README 업데이트, Providers 컴포넌트 재구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import Header from '@/app/components/layout/Header';
 import CategoryBar from '@/app/components/layout/CategoryBar';
-import { Providers } from './providers';
+import { Providers } from '@/providers';
 import VoiceCommandButton from './components/common/VoiceCommandButton';
 import Footer from '@/app/components/layout/Footer';
 import ChatCommandInput from './components/common/ChatCommandInput';
